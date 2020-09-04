516924ead4590d2a0e1886569fe5a9578be63340
caiseyann <caiseyann4@gmail.com>
2020-09-04 19:32:17 +0300

    adding the password class

--- proj6.py
+++ proj6.py
@@ -5,4 +5,10 @@ if "1" in user_id:
      email = input("Enter your user ID :")
 
      with open("passwords\\email.txt","w") as f:
-         
+         store_email = f.write(email)
+
+     password = input("Enter your user passwords :")
+
+     with open("passwords\\password.txt","w") as f:
+         store_password = f.write(password)
+
