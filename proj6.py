c5fb9496eeadbb48057ea67292dcd37abcfece05
caiseyann <caiseyann4@gmail.com>
2020-09-04 19:28:49 +0300

    using the with open command to build app

--- proj6.py
+++ proj6.py
@@ -1,3 +1,8 @@
 user_id = input("Enter your user ID (Press 1 to create account) :")
 
-if "1" in user_id
+if "1" in user_id:
+
+     email = input("Enter your user ID :")
+
+     with open("passwords\\email.txt","w") as f:
+         
