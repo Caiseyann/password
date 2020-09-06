dc33f381acf8dfa01eb2990eaa3ae81f7b70d451
caiseyann <caiseyann4@gmail.com>
2020-09-06 14:50:34 +0300

    unresolved errors looking for new direction, deleting files

--- proj6.py
+++ proj6.py
@@ -1,31 +1,31 @@
 import pickle
 import pyperclip
 
-user_id = input("Enter your user ID (Press 1 to create account) :")
+user_id = input("Enter your user ID (Press 1 to create account) : ")
 
 dictionary = {}
 
-# with open(r"C:\Users\Caiseyann\Documents\passwords\pass_man.txt", "br") as filewrite:
-#     dictionary = pickle.load(filewrite)
+with open(r"C:\Users\Caiseyann\Documents\passwords\pass_man.txt", "br") as filewrite:
+    dictionary = pickle.load(filewrite)
 
 if "1" in user_id:
 
-     email = input("Enter your user ID :")
+     email = input("Enter your user ID : ")
 
-     with open(r"C:\Users\Caiseyann\Documents\passwords\email.txt", "w") as f:
-         store_email = f.write(email)
+     with open(r"C:\\Users\\Caiseyann\\Documents\\passwords\\email.txt", "w") as f:
+        f.write(email)
 
-     password = input("Enter your user passwords :")
+     password = input("Enter your user passwords : ")
 
-     with open(r"C:\Users\Caiseyann\Documents\passwords\pass.txt", "w") as a:
-         store_pass = a.write(password)
+     with open(r"C:\\Users\\Caiseyann\\Documents\\passwords\\pass.txt", "w") as a:
+         a.write(password)
 
-with open(r"C:\Users\Caiseyann\Documents\passwords\email.txt", "r") as fr:
+with open(r"C:\\Users\\Caiseyann\\Documents\\passwords\\email.txt", "r") as fr:
          store_email = fr.read()
 
 if user_id == store_email:
     password2 = input("Enter your password : ")
-    with open(r"C:\Users\Caiseyann\Documents\passwords\pass.txt", "r") as ar:
+    with open(r"C:\\Users\\Caiseyann\\Documents\\passwords\\pass.txt", "r") as ar:
         store_pass = ar.read()
 
     if password2 == store_pass:
@@ -35,26 +35,27 @@ if user_id == store_email:
            account = input("Enter your account name : ")
            acc_pass = input("Enter your account password : ")
 
-           confirmation = input("Would you like to save this input (y/n) : ")
+           confirmation = input("Would you like to save it (y/n) : ")
 
            if "y" in confirmation:
                dictionary[account] = acc_pass
 
-               with open(r"C:\Users\Caiseyann\Documents\passwords\pass_man.txt", "bw") as readfile:
+               with open(r"C:\\Users\\Caiseyann\\Documents\\passwords\\pass_man.txt", "bw") as readfile:
                    dictionary = pickle.dump(dictionary, readfile, protocal=2)
-               print("Done! Your {account)'s password has been saved")
+               print(f"Done! Your {account}'s password has been saved")
 
            else:
                print("your data has not been saved")
 
-       if "2" in conf:
+       if "1" in conf:
            email1 = input("Which account's password would you want to know: ")
 
-           with open(r"C:\Users\Caiseyann\Documents\passwords\pass_man.txt", "br") as file:
+           with open(r"C:\\Users\\Caiseyann\\Documents\\passwords\\pass_man.txt", "br") as file:
                dictionary = pickle.load(file)
 
-           if email in dictionary:
+           if email1 in dictionary:
                print(f"Your {email1}'s password is {dictionary[email1]}")
+               print("Your password has been saved to your clipboard")
                pyperclip.copy(dictionary[email1])
        else:
            print("This password is not saved")
